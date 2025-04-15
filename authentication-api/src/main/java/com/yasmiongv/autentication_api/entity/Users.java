9c4ad96cdebad00ff7f0f40e2f09216662b8502e
Yasmin Ventura <yasmin.gomsv@gmail.com>
2025-04-15 15:37:16 -0300

    create user entity

---
+++ authentication-api/src/main/java/com/yasmiongv/autentication_api/entity/Users.java
@@ -0,0 +1,70 @@
+package com.yasmiongv.autentication_api.entity;
+
+import com.yasmiongv.autentication_api.entity.enums.Role;
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public Users() {
+    }
+
+    public Users(String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
---
+++ authentication-api/src/main/java/com/yasmiongv/autentication_api/entity/enums/Role.java
@@ -0,0 +1,5 @@
+package com.yasmiongv.autentication_api.entity.enums;
+
+public enum Role {
+    ADMIN, CUSTOMER
+}
