d4182573e2c7d5c9507696b4f504e9ef5cf2ee91
Yasmin Ventura <yasmin.gomsv@gmail.com>
2025-04-16 16:40:54 -0300

    implement user repository

--- authentication-api/src/main/java/com/yasmiongv/autentication_api/service/UserService.java
+++ authentication-api/src/main/java/com/yasmiongv/autentication_api/service/UserService.java
@@ -3,19 +3,22 @@ package com.yasmiongv.autentication_api.service;
 import com.yasmiongv.autentication_api.controller.dtos.UserDTO;
 import com.yasmiongv.autentication_api.controller.mappers.UserMapper;
 
+import com.yasmiongv.autentication_api.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserMapper mapper;
+    private final UserRepository repository;
 
-    public UserService(UserMapper mapper) {
+    public UserService(UserMapper mapper, UserRepository repository) {
         this.mapper = mapper;
+        this.repository = repository;
     }
 
-    public UserDTO register(UserDTO user) {
+    public void register(UserDTO user) {
         var entity = mapper.toEntity(user);
-        return user;
+        repository.save(entity);
     }
 }
