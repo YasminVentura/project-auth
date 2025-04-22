628b0a9e75b1d6e97ed6cba918aa876661839548
Yasmin Ventura <yasmin.gomsv@gmail.com>
2025-04-22 16:04:38 -0300

    Add MethodArgumentNotValidException handling

--- authentication-api/src/main/java/com/yasmiongv/autentication_api/exceptions/ApiExceptionHandler.java
+++ authentication-api/src/main/java/com/yasmiongv/autentication_api/exceptions/ApiExceptionHandler.java
@@ -3,8 +3,11 @@ package com.yasmiongv.autentication_api.exceptions;
 import com.yasmiongv.autentication_api.exceptions.custom.DuplicateEmailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ApiExceptionHandler {
@@ -16,4 +19,17 @@ public class ApiExceptionHandler {
                 ex.getMessage());
         return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidationException(MethodArgumentNotValidException ex) {
+        String msg = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> error.getField() + ": " + error.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        ErrorResponse error = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), msg);
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
+
 }
